8ebe690e3dd5ccbef82bcb6f5bd49d41b02dee92
zura0z <zuramazmishvili@gmail.com>
2020-10-23 15:36:00 +0400

    resize window toggle burger menu

--- script.js
+++ script.js
@@ -4,5 +4,13 @@ const toggle = () => {
     document.getElementsByTagName("body")[0].style.overflow = "visible";
   } else document.getElementsByTagName("body")[0].style.overflow = "hidden";
 };
+const resize = () => {
+  if (
+    window.innerWidth > 850 &&
+    document.getElementById("burger-menu").classList[0] !== "hide"
+  )
+    document.getElementById("burger-menu").classList.toggle("hide");
+};
 
+window.onresize = resize;
 document.getElementById("burger").onclick = toggle;
